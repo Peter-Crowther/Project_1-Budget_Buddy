6fc5b4814cd7eb2a54c999ece688a4b896b519c1
Peter Crowther <petercrowther90@hotmail.com>
2017-10-21 13:56:01 +0100

    created merchants and transactions tables

--- db/budget.sql
+++ db/budget.sql
@@ -1,7 +1,25 @@
+DROP TABLE transactions;
 DROP TABLE tags;
+DROP TABLE merchants;
+
+
 
 CREATE TABLE tags
 (
-  id SERIAL8 PRIMARY KEY,
+  id SERIAL4 PRIMARY KEY,
   tag_name VARCHAR(255)
-)
+);
+
+CREATE TABLE merchants
+(
+  id SERIAL4 PRIMARY KEY,
+  merchant_name VARCHAR(255)
+);
+
+CREATE TABLE transactions
+(
+  id SERIAL4 PRIMARY KEY,
+  amount INT4,
+  merchant_id INT4 REFERENCES merchants(id),
+  tag_it INT4 REFERENCES tags(id)
+);
