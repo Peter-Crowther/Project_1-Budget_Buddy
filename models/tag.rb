db0a6d3669081d071382ef8a2f0618c59737c17d
Peter Crowther <petercrowther90@hotmail.com>
2017-10-21 16:32:39 +0100

    created all and find method for tag

--- models/tag.rb
+++ models/tag.rb
@@ -18,13 +18,20 @@ class Tag
     @id = results.first()["id"].to_i()
   end
 
-  def self.all(id)
+  def self.all()
     sql = "SELECT * FROM tags"
     values = []
     results = SqlRunner.run(sql, values)
     return results.map{|tag|Tag.new(tag)}
   end
 
+  def self.find(id)
+    sql = "SELECT * FROM tags
+          WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    return = Tag.new(results.first)
+  end
 
 
 end
